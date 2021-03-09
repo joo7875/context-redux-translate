72b2c84040a890d4763c977aceac2cbd9d62316c
Hyewon Joo <studiomate@studiomateui-MacBookPro.local>
2021-03-09 14:18:12 +0900

    wrapping up Context

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,37 @@ import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
+import { LanguageStore } from '../contexts/LanguageContext';
 
 class App extends React.Component {
 
-  state ={ language: 'english' };
+  // state ={ language: 'english' };
 
-  // callback method we pass into some data
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
+  // // callback method we pass into some data
+  // onLanguageChange = language => {
+  //   this.setState({ language });
+  // };
 
   render() {
     return (
       <div className='ui container'>
-        <div>
-          Select a language:
-          <i className='flag us' onClick={() => this.onLanguageChange('english')} />
-          <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
-        </div>
+        <LanguageStore>
+          <LanguageSelector />
+
+          <ColorContext.Provider value='red'>
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+
+        {/* <LanguageSelector onLanguageChange={this.onLanguageChange} /> */}
 
         {/* doesn't matter if you put the context component inside or outside of a component */}
-        <ColorContext.Provider value='red'>
+        {/* <ColorContext.Provider value='red'>
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
           </LanguageContext.Provider>
-        </ColorContext.Provider>
+        </ColorContext.Provider> */}
       </div>
     );
   }
